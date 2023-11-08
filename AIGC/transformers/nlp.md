c66f43f61f674bf5815b7036ad8359f866edbd80
feng-duping <2292241828@qq.com>
2023-11-08 21:38:08 +0800

    '图片识别'

---
+++ AIGC/transformers/nlp.md
@@ -0,0 +1,18 @@
+# 机器学习任务
+transformers huggingface的核心库
+- pipeline 分发任务
+    pipeline(task, model)
+- 数组和hashTable|Dict
+    - 相同点
+       数据容器 分为 线性 | 非线性
+    - 区别
+        数组，Dict和栈 队列 链表  区别在于连续（eg.数组）和不连续，指的是内存空间，
+        非线性 树 图
+        查找线性容器里 
+- transformers 支持哪些nlp任务
+    SUPPORTED_TASKS 常量
+    items 值是一个Dict JS JSON
+    for k,v in
+CPU的加法运算最快，for技术循环最快    for in   for of
+图解HTTP协议这本书可以读下，很薄
+计算机组成原理（想要面试字节得话）
--- js/this/test.js
+++ js/this/test.js
@@ -1,2 +1,37 @@
-var a = 1
-console.log(global)
+
+// function foo(){
+//     var a = 1
+//     function bar(){
+//         console.log(this.a);
+//     }
+//     var baz = bar.bind(foo)
+//     baz()
+// }
+// foo()
+// var a = 1
+// var bar = function(){
+//     console.log(this.a);
+// }
+// bar()
+// var obj = {
+//     name: 'Tom',
+//     show: function(){
+
+//         // console.log(this.name)
+//         var bar = function(){
+//             console.log(this.name)
+//         }
+//         bar()
+//     }
+// }
+// obj.show()
+var obj = {
+    name: 'Tom',
+    show: function(){
+        var bar = () => {
+            console.log(this.name)
+        }
+        bar()
+    }
+}
+obj.show()
